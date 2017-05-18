34e7e2a577f60882a65ad272cca7327062a1790d
Viktor Holmgren <vikktror@hotmail.com>
2017-05-18 22:08:37 +0200

    TEsting out a little bit

---
+++ Tick.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   Tick.h
+ * Author: Viktor
+ *
+ * Created on den 5 mars 2017, 17:01
+ */
+
+#include "DataTypes.h"
+
+#ifndef TICK_H
+#define	TICK_H
+
+typedef enum
+{
+    OFF = 0,
+    ON
+}TICK_STATUS;
+
+vo voTickTask(u8 u8Delay);
+vo voTickReset(vo);
+TICK_STATUS enGetTick(vo);
+
+#endif	/* TICK_H */
+
